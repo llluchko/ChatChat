3c9642de1bcf16456fb10ca7b0f60ce1f66c8434
llluchko <l.mladenov21@gmail.com>
2016-11-05 21:15:09 +0200

    Save sent and received messages in Firebase

--- ChatChat/ChatViewController.swift
+++ ChatChat/ChatViewController.swift
@@ -22,6 +22,8 @@ final class ChatViewController: JSQMessagesViewController {
 	var messages = [JSQMessage]()
 	lazy var outgoingBubbleImageView: JSQMessagesBubbleImage = self.setupOutgoingBubble()
 	lazy var incomingBubbleImageView: JSQMessagesBubbleImage = self.setupIncomingBubble()
+	private lazy var messageRef: FIRDatabaseReference = self.channelRef!.child("messages")
+	private var newMessageRefHandle: FIRDatabaseHandle?
 	
     // MARK: View Lifecycle
 	override func viewDidLoad() {
@@ -30,18 +32,7 @@ final class ChatViewController: JSQMessagesViewController {
 		self.senderId = FIRAuth.auth()?.currentUser?.uid
 		collectionView!.collectionViewLayout.incomingAvatarViewSize = CGSize.zero
 		collectionView!.collectionViewLayout.outgoingAvatarViewSize = CGSize.zero
-	}
-	
-	override func viewDidAppear(_ animated: Bool) {
-		super.viewDidAppear(animated)
-		
-		// messages from someone else
-		addMessage(withId: "foo", name: "Mr.Bolt", text: "I am so fast!")
-		// messages sent from local sender
-		addMessage(withId: senderId, name: "Me", text: "I bet I can run faster than you!")
-		addMessage(withId: senderId, name: "Me", text: "I like to run!")
-		// animates the receiving of a new message on the view
-		finishReceivingMessage()
+		observeMessages()
 	}
 	
     // MARK: Collection view data source (and related) methods
@@ -80,6 +71,37 @@ final class ChatViewController: JSQMessagesViewController {
   
   
 	// MARK: Firebase related methods
+	override func didPressSend(_ button: UIButton!, withMessageText text: String!, senderId: String!, senderDisplayName: String!, date: Date!) {
+		let itemRef = messageRef.childByAutoId()
+		let messageItem = [
+			"senderId": senderId!,
+			"senderName": senderDisplayName!,
+			"text": text!,
+		]
+		
+		itemRef.setValue(messageItem)
+		JSQSystemSoundPlayer.jsq_playMessageSentSound()
+		finishSendingMessage()
+	}
+	
+	private func observeMessages() {
+		messageRef = channelRef!.child("messages")
+		
+		let messageQuery = messageRef.queryLimited(toLast: 25)
+		
+		// We can use the observe method to listen for new
+		// messages being written to the Firebase DB
+		newMessageRefHandle = messageQuery.observe(.childAdded, with: { (snapshot) -> Void in
+			let messageData = snapshot.value as! Dictionary<String, String>
+			
+			if let id = messageData["senderId"] as String!, let name = messageData["senderName"] as String!, let text = messageData["text"] as String!, text.characters.count > 0 {
+				self.addMessage(withId: id, name: name, text: text)
+				self.finishReceivingMessage()
+			} else {
+				print("Error! Could not decode message data")
+			}
+		})
+	}
 
 
 	// MARK: UI and User Interaction
